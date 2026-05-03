12f307a3dc2f0778f059f9215c0b537a34466fd4
jaggannadhan <jegsirox@example.com>
2026-05-03 11:40:48 -0400

    news reader files

---
+++ data/processed/news_reader/run_20260503_0217/_shim/sitecustomize.py
@@ -0,0 +1,3 @@
+import numpy as np
+for attr, val in [('float',float),('int',int),('complex',complex),('bool',bool),('object',object),('str',str)]:
+    if not hasattr(np, attr): setattr(np, attr, val)
